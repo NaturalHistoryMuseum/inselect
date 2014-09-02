5781c36567aac9bd41d126d641be1e7cafdac1de
Stefan van der Walt <stefan@sun.ac.za>
2014-09-02 17:12:19 +0100

    Add basic GUI instantiation test

---
+++ inselect/tests/test_basic.py
@@ -0,0 +1,16 @@
+from inselect.image_viewer import ImageViewer
+
+import os
+import sys
+
+from PySide import QtGui
+
+import numpy as np
+from skimage import data_dir
+
+
+def test_main():
+    app = QtGui.QApplication(sys.argv)
+    window = ImageViewer()
+    window.open(os.path.join(data_dir, 'chelsea.png'))
+    window.segment()
