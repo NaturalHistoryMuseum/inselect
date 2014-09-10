71fa8418b8127f4d9338d514f492d2e462976cc5
Pieter Holtzhausen <holtzhau@gmail.com>
2014-09-10 13:25:20 +0100

    Json fix

--- inselect/image_viewer.py
+++ inselect/image_viewer.py
@@ -155,6 +155,8 @@ class ImageViewer(QtGui.QMainWindow):
             self.export_action.setEnabled(True)
             self.zoom_in_action.setEnabled(True)
             self.zoom_out_action.setEnabled(True)
+            self.save_action.setEnabled(True)
+            self.import_action.setEnabled(True)
 
     def zoom_in(self):
         self.view.set_scale(1.2)
@@ -186,6 +188,7 @@ class ImageViewer(QtGui.QMainWindow):
         b = box.boundingRect()
         box.setZValue(max(1000, 1E9 - b.width() * b.height()))
         box.updateResizeHandles()
+        return box
 
     def worker_finished(self, rects, display):
         self.progressDialog.hide()
@@ -320,48 +323,50 @@ class ImageViewer(QtGui.QMainWindow):
             statusTip="Export",
             triggered=self.export)
 
-    def save_boxes(self):
-        file_name, filtr = QtGui.QFileDialog.getSaveFileName(
-            self,
-            "QFileDialog.getSaveFileName()",
-            self.file_name + ".csv",
-            "All Files (*);;CSV Files (*.csv)", "",
-            QtGui.QFileDialog.Options())
-        if file_name:
-            with open(file_name, 'w') as csvfile:
-                writer = csv.writer(csvfile, delimiter=' ')
-                for item in self.view.items:
-                    rect = item.rect()
-                    box = [rect.left(), rect.top(),
-                           rect.width(), rect.height()]
-                    width = self.image_item.pixmap().width()
-                    height = self.image_item.pixmap().height()
-                    box[0] /= width
-                    box[1] /= height
-                    box[2] /= width
-                    box[3] /= height
-                    writer.writerow(box)
-
     def import_boxes(self):
         files, filtr = QtGui.QFileDialog.getOpenFileNames(
             self,
-            "QFileDialog.getOpenFileNames()", "../data",
-            "All Files (*);;Text Files (*.csv)", "",
+            "QFileDialog.getOpenFileNames()", "data",
+            "All Files (*);;Text Files (*.json)", "",
             QtGui.QFileDialog.Options())
 
         if files:
             width = self.image_item.pixmap().width()
             height = self.image_item.pixmap().height()
             for file_name in files:
-                with open(file_name, 'r') as csvfile:
-                    reader = csv.reader(csvfile, delimiter=' ')
-                    for row in reader:
-                        rect = [float(x) for x in row]
-                        rect[0] *= width
-                        rect[1] *= height
-                        rect[2] *= width
-                        rect[3] *= height
-                        self.add_box(rect)
+                data = json.load(open(file_name))
+                for item in data["items"]:
+                    rect = [float(x) for x in item["rect"]]
+                    rect[0] *= width
+                    rect[1] *= height
+                    rect[2] *= width
+                    rect[3] *= height
+                    box = self.add_box(rect)
+                    box.list_item.fields = item["fields"]
+
+    def save_boxes(self):
+        file_name, filtr = QtGui.QFileDialog.getSaveFileName(
+            self,
+            "QFileDialog.getSaveFileName()",
+            self.filename + ".json",
+            "All Files (*);;json Files (*.json)", "",
+            QtGui.QFileDialog.Options())
+        if file_name:
+            data = {'image_name': self.filename}
+            data["items"] = []
+            for i, box in enumerate(self.view.items):
+                rect = box.rect()
+                rect = [rect.left(), rect.top(),
+                        rect.width(), rect.height()]
+                width = self.image_item.pixmap().width()
+                height = self.image_item.pixmap().height()
+                rect[0] /= width
+                rect[1] /= height
+                rect[2] /= width
+                rect[3] /= height
+                data['items'].append({'rect': rect,
+                                      'fields': box.list_item.fields})
+            json.dump(data, open(file_name, "w"), indent=4)
 
     def create_menus(self):
         self.toolbar = self.addToolBar("Edit")
