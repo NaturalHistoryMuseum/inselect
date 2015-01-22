a48bdc428468ee14b4c2c8f6692e4a44c2a6a047
Lawrence Hudson <quicklizard@googlemail.com>
2015-01-22 15:52:14 +0000

    Version string is three numbers separated by dots, for Windows build

--- inselect/__init__.py
+++ inselect/__init__.py
@@ -1,1 +1,4 @@
-__version__ = '0.1.4a2'
+__version__ = '0.1.4'
+# distutils/version.py and win32/lib/win32versionstamp.py have different
+# restrictions on the format of version numbers - both must be met.
+# Three numbers separated by dots is a safe format.
