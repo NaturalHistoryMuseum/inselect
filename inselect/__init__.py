4500f27d81b8950b03801038b4dc537834673f6c
Lawrence Hudson <quicklizard@googlemail.com>
2015-01-23 10:40:21 +0000

    Bump version

--- inselect/__init__.py
+++ inselect/__init__.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.4'
+__version__ = '0.1.5'
 # distutils/version.py and win32/lib/win32versionstamp.py have different
 # restrictions on the format of version numbers - both must be met.
 # Three numbers separated by dots is a safe format.
