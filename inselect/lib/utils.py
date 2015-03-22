3daf5fa391805ca241a70856c94348d50c002789
Lawrence Hudson <quicklizard@googlemail.com>
2015-03-22 07:08:20 +0000

    user_name failure cases

--- inselect/lib/utils.py
+++ inselect/lib/utils.py
@@ -19,6 +19,7 @@ except ImportError:
 
 
 try:
+    import pywintypes
     import win32api
 except ImportError:
     win32api = None
@@ -89,9 +90,15 @@ def duplicated(v):
 def user_name():
     """The name of the current user
     """
-    if win32api:
+    if win32api and pywintypes:
         NameDisplay = 3
-        return win32api.GetUserNameEx(NameDisplay)
+        try:
+            return win32api.GetUserNameEx(NameDisplay)
+        except pywintypes.error:
+            try:
+                return win32api.GetUserName()
+            except pywintypes.error:
+                return ''
     elif pwd:
         # Strip trailing commans seen on Linux
         return pwd.getpwuid(os.getuid()).pw_gecos.rstrip(',')
