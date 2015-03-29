557a55cc9aa56778998bccfb53149da1e2cdcb1b
Lawrence Hudson <quicklizard@googlemail.com>
2015-03-29 08:30:30 +0100

    Template-aware

--- inselect/workflow/save_crops.py
+++ inselect/workflow/save_crops.py
@@ -16,15 +16,16 @@ import inselect.lib.utils
 
 from inselect.lib.document import InselectDocument
 from inselect.lib.document_export import DocumentExport
+from inselect.lib.metadata_library import library
 from inselect.lib.utils import debug_print
 
 
 # TODO Recursive option
-# TODO Ignore if existing crops dir; option to overwrite
+# TODO Ignore documents that fail validation; option to ignore failures
 
-def save_crops(dir, overwrite_existing):
+def save_crops(dir, overwrite_existing, metadata_template):
     dir = Path(dir)
-    export = DocumentExport()
+    export = DocumentExport(metadata_template)
     for p in dir.glob('*' + InselectDocument.EXTENSION):
         try:
             debug_print('Loading [{0}]'.format(p))
@@ -45,18 +46,23 @@ def save_crops(dir, overwrite_existing):
 
 
 def main():
-    parser = argparse.ArgumentParser(description='Writes cropped object images from Inselect documents')
+    parser = argparse.ArgumentParser(
+        description='Writes cropped object images from Inselect documents')
     parser.add_argument("dir", help='Directory containing Inselect documents')
     parser.add_argument('-o', '--overwrite', action='store_true',
         help='Overwrite existing crops directories')
     parser.add_argument('-d', '--debug', action='store_true')
+    parser.add_argument('--template',
+        help=('Use a metadata template to validate documents and generate '
+              'filenames of xcrops'), choices=library().keys())
     parser.add_argument('-v', '--version', action='version',
                         version='%(prog)s ' + inselect.__version__)
     args = parser.parse_args()
 
     inselect.lib.utils.DEBUG_PRINT = args.debug
 
-    save_crops(args.dir, args.overwrite)
+    save_crops(args.dir, args.overwrite, library().get(args.template))
+
 
 if __name__=='__main__':
     main()
