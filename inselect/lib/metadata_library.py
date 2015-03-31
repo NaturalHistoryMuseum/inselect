f8f0667a519ac3307e8aa26c501e5ff2c379eacb
Lawrence Hudson <quicklizard@googlemail.com>
2015-03-31 16:07:47 +0100

    Fix metadata template import on OS X

--- inselect/lib/metadata_library.py
+++ inselect/lib/metadata_library.py
@@ -7,26 +7,36 @@ from collections import OrderedDict
 from inselect.lib.metadata import MetadataTemplate
 from inselect.lib.utils import debug_print
 
-_library = None
+from inselect.lib.templates import dwc, price
 
 
-def library():
-    """Returns a list of MetadataTemplate instances
-    """
-    global _library
-    if not _library:
-        _library = _load_library()
-    return _library
+if True:
+    _library = {}
+    for template in [p.template for p in (dwc, price)]:
+        _library[template.name] = template
+    _library = OrderedDict(sorted(_library.iteritems()))
 
-def _load_library():
-    # Import everything inselect.lib.templates that has a 'template' name
-    # that is an instance of MetadataTemplate.
-    # Returns an instance of OrderedDict with items sorted by key.
-    try:
-        templates = importlib.import_module('.lib.templates', 'inselect')
-    except ImportError,e:
-        debug_print(e)
-    else:
+    def library():
+        return _library
+else:
+    # More flexible solution that breaks with frozen build on OS X using
+    # PyInstaller
+
+    _library = None
+
+    def library():
+        """Returns a list of MetadataTemplate instances
+        """
+        global _library
+        if not _library:
+            _library = _load_library()
+        return _library
+
+    def _load_library():
+        # Import everything inselect.lib.templates that has a 'template' name
+        # that is an instance of MetadataTemplate.
+        # Returns an instance of OrderedDict with items sorted by key.
+        templates = importlib.import_module('inselect.lib.templates')
         library = {}
         for loader, name, is_pkg in pkgutil.iter_modules(templates.__path__):
             try:
