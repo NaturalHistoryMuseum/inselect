02b33961942496145ffcc02cef2131b86c1818c0
Lawrence Hudson <quicklizard@googlemail.com>
2015-06-29 07:23:23 +0100

    Corrected typo in docstring

--- inselect/tests/utils.py
+++ inselect/tests/utils.py
@@ -9,7 +9,7 @@ from inselect.lib.utils import rmtree_readonly
 @contextmanager
 def temp_directory_with_files(*paths):
     """A context manager that creates a temporary directory and copies all
-    paths to it. The temporary directory is unlinked then the context is exited.
+    paths to it. The temporary directory is unlinked when the context is exited.
     """
     temp = tempfile.mkdtemp()
     try:
