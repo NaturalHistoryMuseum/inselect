fc8d76d1b795cb0a013481fce73175a58d60f9c6
Lawrence Hudson <quicklizard@googlemail.com>
2015-07-28 15:12:36 +0100

    Bump version

--- inselect/__init__.py
+++ inselect/__init__.py
@@ -1,6 +1,6 @@
 """Segmentation, validation and annotation of images of museum specimens
 """
-__version__ = '0.1.16'
+__version__ = '0.1.17'
 # distutils/version.py and win32/lib/win32versionstamp.py have different
 # restrictions on the format of version numbers - both must be met.
 # Three numbers separated by dots is a safe format.
