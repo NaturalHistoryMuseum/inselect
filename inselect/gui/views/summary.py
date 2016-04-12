244aff9a91bcad69ea0b0d1e690de9c3587fe6ab
Lawrence Hudson <quicklizard@googlemail.com>
2016-04-12 10:25:14 +0100

    Show selected label

--- inselect/gui/views/summary.py
+++ inselect/gui/views/summary.py
@@ -1,4 +1,5 @@
 import locale
+from PySide import QtCore
 from PySide.QtGui import QAbstractItemView, QHBoxLayout, QLabel, QWidget
 
 from inselect.lib.utils import debug_print
@@ -11,15 +12,16 @@ class SummaryView(QAbstractItemView):
         # This view is not visible
         super(SummaryView, self).__init__(None)
 
-        layout = QHBoxLayout()
-
         self.n_boxes = QLabel()
-        layout.addWidget(self.n_boxes)
+        self.n_selected = QLabel()
+        self.selected_label = QLabel()
 
         # Last item has stretch greater than zero to force all labels to be
         # left-aligned
-        self.n_selected = QLabel()
-        layout.addWidget(self.n_selected, stretch=1)
+        layout = QHBoxLayout()
+        layout.addWidget(self.n_boxes)
+        layout.addWidget(self.n_selected)
+        layout.addWidget(self.selected_label, stretch=1)
 
         self.widget = QWidget(parent)
         self.widget.setLayout(layout)
@@ -34,6 +36,12 @@ class SummaryView(QAbstractItemView):
             '{0} selected'.format(locale.format("%d", n, grouping=True))
         )
 
+    def _selected_label(self, selected):
+        if 1 == len(selected):
+            self.selected_label.setText(selected[0].data(QtCore.Qt.DisplayRole))
+        else:
+            self.selected_label.setText('')
+
     def reset(self):
         """QAbstractItemView virtual
         """
@@ -41,6 +49,7 @@ class SummaryView(QAbstractItemView):
         super(SummaryView, self).reset()
         self._n_boxes(self.model().rowCount())
         self._n_selected(0)
+        self._selected_label([])
 
     def setModel(self, model):
         """QAbstractItemView virtual
@@ -54,12 +63,15 @@ class SummaryView(QAbstractItemView):
         """
         debug_print('SummaryView.dataChanged')
         self._n_boxes(self.model().rowCount())
+        self._selected_label(self.selectionModel().selectedIndexes())
 
     def selectionChanged(self, selected, deselected):
         """QAbstractItemView slot
         """
         debug_print('SummaryView.selectionChanged')
-        self._n_selected(len(self.selectionModel().selectedIndexes()))
+        selected = self.selectionModel().selectedIndexes()
+        self._n_selected(len(selected))
+        self._selected_label(selected)
 
     def rowsAboutToBeRemoved(self, parent, start, end):
         """QAbstractItemView slot
