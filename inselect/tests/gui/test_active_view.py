a1e21c66aedc89b90364b26aac455721fb9b1b0a
Lawrence Hudson <quicklizard@googlemail.com>
2016-12-12 12:23:19 +0000

    [#406] how Boxes view on creating new document

--- inselect/tests/gui/test_active_view.py
+++ inselect/tests/gui/test_active_view.py
@@ -1,6 +1,11 @@
 import unittest
 
+from pathlib import Path
+
 from .gui_test import GUITest
+
+
+TESTDATA = Path(__file__).parent.parent / 'test_data'
 
 
 class TestActiveView(GUITest):
@@ -20,7 +25,7 @@ class TestActiveView(GUITest):
 
     def test_select_next_tab(self):
         "Next tab is selected"
-        self.window.show_tab(0)
+        self.window.show_tab(index=0)
         self.window.next_previous_tab(next=True)
         self.assertEqual(1, self.window.views.currentIndex())
 
@@ -33,6 +38,14 @@ class TestActiveView(GUITest):
         self.assertEqual(1, self.window.views.currentIndex())
 
         self.window.next_previous_tab(next=False)
+        self.assertEqual(0, self.window.views.currentIndex())
+
+
+    def test_close(self):
+        "User closes document and Inselect shows Boxes tab"
+        self.window.open_file(path=TESTDATA.joinpath('shapes.inselect'))
+        self.window.show_tab(index=1)
+        self.window.close()
         self.assertEqual(0, self.window.views.currentIndex())
 
 
